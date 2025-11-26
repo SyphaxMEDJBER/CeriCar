e5be4980e11bebacf8173d296e5d28e1f37246b1
SyphaxMEDJBER <syphax.medjber.8@gmail.com>
2025-11-26 21:10:30 +0100

    model/trajet.php okk

---
+++ models/trajet.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+use app\models\voyage;
+
+
+class trajet extends ActiveRecord{
+  public static function tableName(){
+    return 'fredouil.trajet';
+  }
+
+  public function getVoyages(){
+    return $this->hasMany(voyage::class,['trajet'=>'id']);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
